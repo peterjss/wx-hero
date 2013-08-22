2beb53c98e59adcbc0d8703537e7cbcb40d9ef11
Peter Li <peterjsss@gmail.com>
2013-08-22 09:32:41 +0800

    firest commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heroappr1::Application.config.secret_key_base = 'eabdb5ca793203cba7f1ae7d04eff5bed8484d4819865d5e0098c85cb4428cdc44056f3563f29198511ab90a1f3ca294efc35b9427bdd33c68064529b390f059'
