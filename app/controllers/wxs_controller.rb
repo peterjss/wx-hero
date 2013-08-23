23b717989c570951c592bd7fbe087f0be75a14d8
Peter Li <peterjsss@gmail.com>
2013-08-23 16:53:32 +0800

    add weixin_route class

--- app/controllers/wxs_controller.rb
+++ app/controllers/wxs_controller.rb
@@ -11,10 +11,7 @@ class WxsController < ApplicationController
   def reply_text
     puts 'in reply text.'
     #puts params[:xml][:MsgType]
-
-    if params[:xml][:MsgType] == "text"
-      render "echo", :formats => :xml,  :layout => false
-    end
+      render "echo"
   end
 
   private
